7b512039e388912c4dc1cb49512f1e132aaf4d75
Hindol <hindolbanerjee5@gmail.com>
2023-07-16 01:45:03 +0530

    Fixed Work Page

--- src/components/WorkComponents/MoreWork/MoreWork.jsx
+++ src/components/WorkComponents/MoreWork/MoreWork.jsx
@@ -1,38 +1,85 @@
 import React from "react";
 import "./MoreWork.scss";
 import Cat from "../../../assets/cat.jpg";
+import Reason1 from "../../../assets/ServReasons/Reason1.jpg";
+import Reason2 from "../../../assets/ServReasons/Reason2.jpg";
+import Reason3 from "../../../assets/ServReasons/Reason3.jpg";
+import Reason4 from "../../../assets/ServReasons/Reason4.jpg";
 const MoreWork = () => {
   return (
     <div className="moreWork-container">
       <div className="moreWork-layout">
-        <h2 className="diff-title">More of our Works</h2>
+        <h2 className="diff-title">Why Our Services?</h2>
         <div className="row row1">
           <div className="content">
-            <h3>Title</h3>
+            <h3>Cutting-Edge Expertise in Advanced AI Technologies</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur, error mollitia. Accusantium architecto, quos illum
-              dolor, aspernatur vitae quisquam, minus sapiente recusandae saepe
-              corrupti facere earum omnis autem voluptatum ut provident nemo
-              dolores aperiam laboriosam? Dolorem provident molestias totam
-              repellat!
+              At PentaNeuron, we pride ourselves on staying at the forefront of
+              cutting-edge technologies, including Large Language Models,
+              Natural Language Processing (NLP), Computer Vision, generative AI,
+              and Predictive analysis. Our team consists of top-notch AI experts
+              who are passionate about pushing the boundaries of AI
+              capabilities. By choosing our services, you gain access to the
+              latest advancements in AI, ensuring that your business stays ahead
+              of the competition.
             </p>
           </div>
           <div className="image">
-            <img src={Cat} alt="" srcset="" />
+            <img src={Reason1} alt="" srcset="" />
           </div>
         </div>
         <div className="row row2">
           <div className="image">
-            <img src={Cat} alt="" srcset="" />
+            <img src={Reason2} alt="" srcset="" />
           </div>
           <div className="content">
-            <h3>Title</h3>
+            <h3>Customized Solutions Tailored to Your Business</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur, error mollitia. Accusantium architecto, quos illum
-              dolor, aspernatur vitae quisquam, minus sapiente recusandae saepe
-              corrupti facere earum omnis autem voluptatum ut provident nemo
+              We understand that every business has unique requirements and
+              challenges. That's why we emphasize delivering customized AI
+              solutions that are specifically tailored to meet your business
+              needs. Whether you require a powerful Chatbot Development platform
+              to enhance customer interactions, sophisticated predictive
+              analysis tools to optimize decision-making, or any other AI-based
+              service, we work closely with you to understand your objectives
+              and deliver solutions that precisely address your requirements.
+            </p>
+          </div>
+        </div>
+        <div className="row row3">
+          <div className="content">
+            <h3>Seamless Integration and Scalability</h3>
+            <p>
+              Implementing AI technologies can sometimes be complex,
+              particularly when integrating them with existing systems and
+              workflows. Our AI Agency excels in providing seamless integration
+              solutions, ensuring that our services can be easily incorporated
+              into your existing infrastructure. Furthermore, our solutions are
+              designed to scale alongside your business. Whether you're a
+              startup looking for initial AI implementation or an enterprise
+              with extensive data processing needs, we have the expertise to
+              accommodate your growth and scale our services accordingly.
+            </p>
+          </div>
+          <div className="image">
+            <img src={Reason3} alt="" srcset="" />
+          </div>
+        </div>
+        <div className="row row4">
+          <div className="image">
+            <img src={Reason4} alt="" srcset="" />
+          </div>
+          <div className="content">
+            <h3>Focus on Ethical AI and Data Privacy</h3>
+            <p>
+              We recognize the importance of ethical AI practices and prioritize
+              the privacy and security of your data. Our AI Agency adheres to
+              stringent ethical guidelines and data protection protocols,
+              ensuring that your sensitive information remains confidential and
+              secure. We implement robust security measures to safeguard your
+              data throughout its lifecycle. With us, you can trust that your
+              business operations are supported by AI technologies built on a
+              foundation of ethical practices and privacy protection.
             </p>
           </div>
         </div>
--- src/components/WorkComponents/Projects/Projects.jsx
+++ src/components/WorkComponents/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Projects.scss";
+import { servicesList } from "../../../data/servicesList.js";
 const Projects = () => {
   return (
     <div className="projects-container">
@@ -7,14 +8,34 @@ const Projects = () => {
         <h2 className="work-title">Our Work</h2>
         <div className="projects-card">
           <div className="row row-one row1">
-            <div className="card-full"></div>
+            <div className="card card-full card1">
+              <div className="work-card-content">
+                <h3>{servicesList.llm.title}</h3>
+                <p>{servicesList.llm.desc}</p>
+              </div>
+            </div>
           </div>
           <div className="row row-two row2">
-            <div className="card-half"></div>
-            <div className="card-half"></div>
+            <div className="card card-half card2">
+              <div className="work-card-content">
+                <h3>{servicesList.nlp.title}</h3>
+                <p>{servicesList.nlp.desc}</p>
+              </div>
+            </div>
+            <div className="card card-half card3">
+              <div className="work-card-content">
+                <h3>{servicesList.cv.title}</h3>
+                <p>{servicesList.cv.desc}</p>
+              </div>
+            </div>
           </div>
           <div className="row row-one row3">
-            <div className="card-full"></div>
+            <div className="card card-full card4">
+              <div className="work-card-content">
+                <h3>{servicesList.cbd.title}</h3>
+                <p>{servicesList.cbd.desc}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
